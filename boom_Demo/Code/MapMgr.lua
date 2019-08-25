bbd8940c088b01a15af6e06a8304b79c41818f41
yanyoumo <yanyoumo@gmail.com>
2019-08-25 15:14:17 +0800

    把推动的算法优化了一下

--- boom_Demo/Code/MapMgr.lua
+++ boom_Demo/Code/MapMgr.lua
@@ -8,47 +8,50 @@ function initMap()
     end
 end
 
+function isTargetInMap(tragetPos)
+    return (tragetPos[1] > 0 and tragetPos[1] < mapLineCount + 1) and
+               (tragetPos[2] > 0 and tragetPos[2] < mapLineCount + 1)
+end
+
+function isTargetMovable(tragetPos)
+    return (mapData[tragetPos[1]][tragetPos[2]] <= 0) and
+               isTargetInMap(tragetPos)
+end
+
+function pushElement(cCX, cCY, i, j, cascadePush)
+    isPushingX = (cCX == i + 1 and cCY == j and cursor.dx == -1) or
+                     (cCX == i - 1 and cCY == j and cursor.dx == 1)
+    isPushingY = (cCX == i and cCY == j + 1 and cursor.dy == -1) or
+                     (cCX == i and cCY == j - 1 and cursor.dy == 1)
+    if (not isPushingY) and (not isPushingX) then return end
+    local tragetPos = {i + cursor.dx, j + cursor.dy}
+    if isTargetMovable(tragetPos) then
+        mapData[tragetPos[1]][tragetPos[2]] = mapData[i][j]
+        mapData[i][j] = 0
+    else
+        if isPushingY then cursor.dy = 0 end
+        if isPushingX then cursor.dx = 0 end
+        return
+    end
+end
+
+function pullElement(cCX, cCY, i, j)
+    -- TODO
+end
+
 function updateMap_Cursor_pushOnly()
-    ePosX = cursor.cx + 1
-    ePosY = cursor.cy + 1
+    local covtCX = cursor.cx + 1
+    local covtCY = cursor.cy + 1
     for i = 1, mapLineCount do
         for j = 1, mapLineCount do
             if mapData[i][j] > 0 then
-                if ePosX == i and ePosY == j + 1 and cursor.dy == -1 then
-                    if j - 1 > 0 then
-                        mapData[i][j - 1] = mapData[i][j];
-                        mapData[i][j] = 0;
-                    else
-                        cursor.dy = 0;
-                    end
-                end
-                if ePosX == i and ePosY == j - 1 and cursor.dy == 1 then
-                    if j + 1 < mapLineCount + 1 then
-                        mapData[i][j + 1] = mapData[i][j];
-                        mapData[i][j] = 0;
-                    else
-                        cursor.dy = 0;
-                    end
-                end
-                if ePosX == i+1 and ePosY == j and cursor.dx == -1 then
-                    if i-1 > 0 then
-                        mapData[i-1][j] = mapData[i][j];
-                        mapData[i][j] = 0;
-                    else
-                        cursor.dx = 0;
-                    end
-                end
-                if ePosX == i-1 and ePosY == j and cursor.dx == 1 then
-                    if i+1 < mapLineCount + 1 then
-                        mapData[i+1][j] = mapData[i][j];
-                        mapData[i][j] = 0;
-                    else
-                        cursor.dx = 0;
-                    end
-                end
+                pushElement(covtCX, covtCY, i, j)
             end
         end
     end
 end
 
-function setUpMap() mapData[2][3] = 1 end
+function setUpMap()
+    mapData[2][3] = 1
+    mapData[4][5] = 1
+end
