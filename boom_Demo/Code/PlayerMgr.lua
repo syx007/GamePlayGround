4a4de9127d53a724331fbed56025e18910ee7ab4
yanyoumo <yanyoumo@gmail.com>
2019-09-02 23:18:59 +0800

    Update PlayerMgr.lua
    刚才Merge丢了一个函数，给捞回来

--- boom_Demo/Code/PlayerMgr.lua
+++ boom_Demo/Code/PlayerMgr.lua
@@ -2,6 +2,16 @@ require("Code/InputMgr");
 require("Code/AnimationSheet");
 --all info and operation relate to player here
 
+function initCursor()
+    cursor = {}
+    cursor.cx = 0
+    cursor.cy = 0
+    cursor.dx = 0
+    cursor.dy = 0
+    cursor.action = 0
+    cursor.rotate = 0
+end
+
 function initPlayer()
 	--this is infact a table in LUA
 	--however we could use this just like a struct
