2bb1f7a83493a77321ddbd398c3b2db4fb30d995
Titianzi <tz930824@outlook.com>
2019-09-08 07:25:03 +0800

    修复了一个drawTile函数的bug

--- boom_Demo/Code/Graphic/GraphicMgr.lua
+++ boom_Demo/Code/Graphic/GraphicMgr.lua
@@ -128,8 +128,8 @@ end
 function drawTile(grid_x,grid_y,core_id,side_id,rot)
     love.graphics.setColor( 1, 1, 1, 1)
     local coord=getWorldCoordfromGrid(grid_x,grid_y,camera_bias_x,camera_bias_y)
-    love.graphics.draw(Tiles[core_id][side_id].Side.content,coord.x+16,coord.y+16,(rot-1)*(3.141592654/2),ZoomFactor*0.95,ZoomFactor*0.95,16,16)
-    love.graphics.draw(Tiles[core_id][side_id].Core.content,coord.x+16,coord.y+16,0,ZoomFactor*0.95,ZoomFactor*0.95,16,16)
+    love.graphics.draw(Tiles[core_id][side_id].Side.content,coord.x+ZoomFactor*16,coord.y+ZoomFactor*16,(rot-1)*(3.141592654/2),0.95*ZoomFactor,0.95*ZoomFactor,16,16)
+    love.graphics.draw(Tiles[core_id][side_id].Core.content,coord.x+ZoomFactor*16,coord.y+ZoomFactor*16,0,0.95*ZoomFactor,0.95*ZoomFactor,16,16)
 end
 function updateTileMap()
     for i = 1, mapLineCount do
@@ -281,8 +281,10 @@ function drawPlayingGame()
     DrawMapLayer1(map_size.w,map_size.h,0.4,0.4,0.4,1)
     --drawGrid(mapLineCount, mapULoffsetX, mapULoffsetY, mapSize, mapSize)
     updateTileMap()
-    print(cursor.cx,cursor.cy)
-    drawTile(3,3,1,1,0)
+    --print(cursor.cx,cursor.cy)
+
+    drawTile(3,3,1,1,0)--Draw PCB Core with PCB connector in grid pos 3,3
+
     local grid_coord=MapIdx2GridCoord(cursor.cx,cursor.cy)
     drawCursor(grid_coord.x,grid_coord.y,1,0,0,0.5*math.sin(0.1*t)+1)
     --updateAnimation()
