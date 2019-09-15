b8453f9e497deb64750a0cf333f26d0aae5dbec6
Titianzi <tz930824@outlook.com>
2019-09-15 20:09:45 +0800

    字符雨效果完成

--- boom_Demo/Code/Resource/ResourceMgr.lua
+++ boom_Demo/Code/Resource/ResourceMgr.lua
@@ -96,6 +96,8 @@ function loadUIResource()
     incomeUI = love.graphics.newImage("Art/UI/Income.png")
     arrowUI = love.graphics.newImage("Art/UI/arrow.png")
     EnterTipsUI=love.graphics.newImage("Art/UI/pressAtostart.png")
+    menuBackground=love.graphics.newImage("Art/UI/background.png")
+    menuLogo=love.graphics.newImage("Art/UI/logo.png")
     
 end
 
